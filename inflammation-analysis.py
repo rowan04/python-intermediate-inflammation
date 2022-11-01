f1e9852e26a8fba1f5ed9f38f86451ebebb85d64
rowan04 <rowanmoss04@gmail.com>
2022-11-01 12:22:56 +0000

    Further style changes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -23,9 +23,11 @@ def main(args):
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 
+        view_data = {
+        'average': models.daily_mean(inflammation_data), 
         'max': models.daily_max(inflammation_data), 
-        'min': models.daily_min(inflammation_data)}
+        'min': models.daily_min(inflammation_data)
+        }
 
         views.visualize(view_data)
 
