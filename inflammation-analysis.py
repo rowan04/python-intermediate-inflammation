ef324baf40d4963f1d902d4fe3279aa3b3b87547
rowan04 <rowanmoss04@gmail.com>
2022-11-01 12:18:36 +0000

    style changes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Software for managing and analysing patients' inflammation data in our imaginary hospital."""
+"""Software for managing and analysing patients' 
+inflammation data in our imaginary hospital."""
 
 import argparse
 
@@ -13,15 +14,18 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
+
     InFiles = args.infiles
+
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
-
 
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data), 
+        'max': models.daily_max(inflammation_data), 
+        'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
 
