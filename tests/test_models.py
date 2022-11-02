bcdd4af5791badc40ee838bfc9b5c28bd45d49ae
rowan04 <rowanmoss04@gmail.com>
2022-11-02 12:28:05 +0000

    test for patient_normalise function

--- tests/test_models.py
+++ tests/test_models.py
@@ -50,3 +50,15 @@ def test_daily_min_string():
     from inflammation.models import daily_min
     with pytest.raises(TypeError):
         error_expected = daily_min([['abd', 'ads'], ['asd', 'auhs']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ]
+)
+def test_patient_normalise(test, expected):
+    '''Test normalisation works'''
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
